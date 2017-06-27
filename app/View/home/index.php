791fab51ca0659623e342e572d597353bbbbfff9
Alexis Bachelet <app.bachelet@gmail.com>
2017-06-28 00:18:27 +0200

    Added some text in home page

--- app/View/home/index.php
+++ app/View/home/index.php
@@ -1,6 +1,15 @@
 <div class="jumbotron">
     <div class="container">
         <h1>Accueil</h1>
-        <h3>Bienvenue sur le site web du Garage Reugeot, Garage de père en fils !</h3>
+        <h3>Bienvenue sur le site web du Garage Reugeot !</h3>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <p>Si vous souhaitez parcourir la liste de nos véhicules, c'est <a href="<?= WEB_ROOT . '/vehicules'?>" class="btn btn-sm btn-primary">Par ici !</a></p>
+            <p>N'hésitez pas également à vous inscrire en cliquant sur le bouton S'inscrire en haut à droite de la page !</p>
+        </div>
     </div>
 </div>
